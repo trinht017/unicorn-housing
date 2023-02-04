cc6331c1cd6dd44aefa8e1211d5fbe07f47f7dc7
Trinh Tran <trinht@188.133.21.198.guest.wifi.dyn.clemson.edu>
2023-02-04 16:56:28 -0500

    auth0 setup

--- unicorn-express/index.js
+++ unicorn-express/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+require('dotenv').config();
+
+const { auth } = require('express-openid-connect');
+
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: process.env.SECRET,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  console.log(req);
+  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 });
 
 const port = process.env.PORT || 3000;
