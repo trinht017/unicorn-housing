a59955ba8d6383b90ca86eb21a0c2169b8f6c5c0
Trinh Tran <trinht@188.133.21.198.guest.wifi.dyn.clemson.edu>
2023-02-04 23:20:55 -0500

    fix stuffs

--- unicorn-express/index.js
+++ unicorn-express/index.js
@@ -3,6 +3,22 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const { auth } = require('express-oauth2-jwt-bearer');
 const guard = require('express-jwt-permissions')();
+const cors = require('cors');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const axios = require('axios');
+
+const verifyJwt = jwt.expressjwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://dev-ix13ko5ij4ojfhls.us.auth0.com/.well-known/jwks.json',
+  }),
+  audience: 'https://unicron-api.com',
+  issuer: process.env.ISSUER_BASE_URL,
+  algorithms: ['RS256'],
+});
 
 const dbUrl = process.env.ATLAS_URI;
 mongoose.connect(dbUrl, {
@@ -17,25 +33,27 @@ db.once('open', () => {
 });
 
 const app = express();
+app.use(cors());
 
-const jwtCheck = auth({
-  audience: 'https://unicron-api.com',
-  issuerBaseURL: 'https://dev-ix13ko5ij4ojfhls.us.auth0.com/',
-  tokenSigningAlg: 'HS256',
+app.get('/chicken', (req, res) => {
+  res.send('hello from chicken');
 });
 
+app.get('/protected', (req, res) => {
+  res.send('protected chunk');
+});
+
+// const jwtCheck = auth({
+//   audience: 'https://unicron-api.com',
+//   issuerBaseURL: 'https://dev-ix13ko5ij4ojfhls.us.auth0.com/',
+//   tokenSigningAlg: 'RS256',
+// });
+
 // enforce on all endpoints
-app.use(jwtCheck);
+// app.use(jwtCheck);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get('/', guard.check(['read:challenges']), (req, res) => {
-  res.json({
-    hello: 'this is the first one',
-    hi: 'this is the second one',
-  });
-});
 
 const postingRoutes = require('./routes/postings');
 app.use('/postings', postingRoutes);
--- unicorn-express/routes/postings.js
+++ unicorn-express/routes/postings.js
@@ -1,7 +1,64 @@
 const express = require('express');
 const router = express.Router();
-
-router.route('/').get();
+let list = [
+  {
+    id: 1,
+    cover: '../images/p-1.png',
+    name: 'Red Carpet Real Estate',
+    location: '210 Zirak Road, Canada',
+    category: 'For Rent',
+    price: '$3,700',
+    type: 'Apartment',
+  },
+  {
+    id: 2,
+    cover: '../images/p-2.png',
+    name: 'Fairmount Properties',
+    location: '5698 Zirak Road, NewYork',
+    category: 'For Sale',
+    price: '$9,750',
+    type: 'Condos',
+  },
+  {
+    id: 3,
+    cover: '../images/p-7.png',
+    name: 'The Real Estate Corner',
+    location: '5624 Mooker Market, USA',
+    category: 'For Rent',
+    price: '$5,860',
+    type: 'Offices',
+  },
+  {
+    id: 4,
+    cover: '../images/p-4.png',
+    name: 'Herringbone Realty',
+    location: '5621 Liverpool, London',
+    category: 'For Sale',
+    price: '$7,540',
+    type: 'Homes & Villas',
+  },
+  {
+    id: 5,
+    cover: '../images/p-5.png',
+    name: 'Brick Lane Realty',
+    location: '210 Montreal Road, Canada',
+    category: 'For Rent',
+    price: '$4,850',
+    type: 'Commercial',
+  },
+  {
+    id: 6,
+    cover: '../images/p-6.png',
+    name: 'Banyon Tree Realty',
+    location: '210 Zirak Road, Canada',
+    category: 'For Sale',
+    price: '$2,742',
+    type: 'Apartment',
+  },
+];
+router.route('/').get((req, res) => {
+  res.json(list);
+});
 
 router.route('/:id').get();
 
