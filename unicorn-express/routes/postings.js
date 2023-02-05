4c935e183035e4f027162002205dca2e7bdc2124
Trinh Tran <trinht@188.133.21.198.guest.wifi.dyn.clemson.edu>
2023-02-05 03:38:43 -0500

    merge

--- unicorn-express/routes/postings.js
+++ unicorn-express/routes/postings.js
@@ -5,33 +5,20 @@ const S3 = require('../controllers/s3');
 const catchAsync = require('../utils/catchAsync');
 const postings = require('../controllers/postings');
 const { validatePosting } = require('../middleware');
-<<<<<<< HEAD
 const { verifyJwt } = require('../utils/auth');
 router
   .route('/')
-=======
-
-router.route('/')
->>>>>>> 0073a5bc5f0c5ab48244f90508a6d5f8bca8cf84
   .get(catchAsync(postings.index))
   .post(verifyJwt, validatePosting, catchAsync(postings.createPosting));
 
-router.route('/deleteAll')
-  .delete(catchAsync(postings.deleteAllPosting));
+router.route('/deleteAll').delete(catchAsync(postings.deleteAllPosting));
 
-router.route('/:id')
+router
+  .route('/:id')
   .get(catchAsync(postings.showPosting))
   .put(verifyJwt, validatePosting, catchAsync(postings.updatePosting))
   .delete(verifyJwt, catchAsync(postings.deletePosting));
 
-<<<<<<< HEAD
-const S3 = require('../controllers/s3');
-
 router.route('/images').post(S3.uploadImages).get(S3.listImages);
-=======
-router.route('/images')
-  .post(S3.uploadImages)
-  .get(S3.listImages);
->>>>>>> 0073a5bc5f0c5ab48244f90508a6d5f8bca8cf84
 
 module.exports = router;
