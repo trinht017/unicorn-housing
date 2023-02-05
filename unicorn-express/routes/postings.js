ada8fba8a9a7baad5b4c8b2ac72d9eb58262cac9
nolan-hamel <69911789+nolan-hamel@users.noreply.github.com>
2023-02-05 01:24:40 -0500

    fix formatting

--- unicorn-express/routes/postings.js
+++ unicorn-express/routes/postings.js
@@ -1,32 +1,26 @@
 const express = require('express');
 const router = express.Router();
 
+const S3 = require('../controllers/s3');
 const catchAsync = require('../utils/catchAsync');
 const Posting = require('../models/posting');
 const postings = require('../controllers/postings');
 const { validatePosting } = require('../middleware');
 
-router
-  .route('/')
+router.route('/')
   .get(catchAsync(postings.index))
   .post(validatePosting, catchAsync(postings.createPosting));
 
-router.route('/deleteAll').delete(catchAsync(postings.deleteAllPosting));
+router.route('/deleteAll')
+  .delete(catchAsync(postings.deleteAllPosting));
 
-router
-  .route('/:id')
+router.route('/:id')
   .get(catchAsync(postings.showPosting))
   .put(validatePosting, catchAsync(postings.updatePosting))
   .delete(catchAsync(postings.deletePosting));
 
-const S3 = require('../controllers/s3');
-
-//router.route('/').get();
-
-//router.route('/:id').get();
-
-//router.route('/:id/edit');
-
-router.route('/images').post(S3.uploadImages).get(S3.listImages);
+router.route('/images')
+  .post(S3.uploadImages)
+  .get(S3.listImages);
 
 module.exports = router;
